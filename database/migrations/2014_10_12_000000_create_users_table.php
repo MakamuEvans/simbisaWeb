71383b2e1246f50ecb17b936b0bd6bae52d69f0e
Makamu Evans <makamuevans@gmail.com>
2018-05-03 13:58:26 +0300

    theme added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->integer('user_type');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
