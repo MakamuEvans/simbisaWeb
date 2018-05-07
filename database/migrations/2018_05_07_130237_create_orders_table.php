442fb73be2f0758c0e2287b851d0ea3bd800a3d0
Makamu Evans <makamuevans@gmail.com>
2018-05-07 16:26:21 +0300

    api auth

---
+++ database/migrations/2018_05_07_130237_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->integer('location_id')->unsigned();
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+
+            //create table relationship
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('location_id')->references('id')->on('vendor_locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/haa', function (){
+    return \Illuminate\Support\Facades\Response::json(\App\Model\Vendor::all());
+})->middleware('auth:api');
